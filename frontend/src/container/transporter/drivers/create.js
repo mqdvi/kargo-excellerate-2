85f3dd65cef2344b3c262d5099ee16cba3c49aa5
AwwalaK <awwalakamila01@gmail.com>
2022-08-13 15:17:44 +0700

    update create in fe

--- frontend/src/container/transporter/drivers/create.js
+++ frontend/src/container/transporter/drivers/create.js
@@ -7,7 +7,9 @@ export class CreateTransporterDrivers extends Component {
 
         this.state = {
             'phoneNumber': '',
-            'driverName':'',
+            'name':'',
+            'idCard':'',
+            'driverLicense':''
         }
     }
 
@@ -22,13 +24,20 @@ export class CreateTransporterDrivers extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
+    onFileChange = (e) => {
+	    this.setState({ idCard: e.target.files[0] });
+	};
+
     handlerSubmit = async (event) => {
         event.preventDefault()
         
-        const formData = new FormData();
-        formData.append("phoneNumber", this.state.phoneNumber);
-        formData.append("driverName", this.state.driverName);
+        const formData = {};
+        formData["phoneNumber"] = this.state.phoneNumber;
+        formData["name"] = this.state.name;
+        formData["idCard"] = this.state.idCard.name;
+        formData["driverLicense"] = this.state.driverLicense.name;
             
+        // console.log(this.state.idCard.name, formData)
         await DriverService.createDriver(formData);
         window.open("/Transporter/drivers", "_self")
     }
@@ -40,12 +49,21 @@ export class CreateTransporterDrivers extends Component {
 
                 <form class="rounded p-10" onSubmit={this.handlerSubmit}>
                     <div class="mb-6s">
-                        <label class="block text-sm font-header mb-2 uppercase" for="driverName">Driver Name</label>
-                        <input class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="driverName" type="text" placeholder="driverName" onChange = {this.handlerChange} required/>
+                        <label class="block text-sm font-header mb-2 uppercase" for="name">Driver Name</label>
+                        <input class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="name" type="text" placeholder="name" onChange = {this.handlerChange} required/>
                     </div>
                     <div class="mb-6">
                         <label class="block text-sm font-header mb-2 uppercase" for="phoneNumber">Phone Number</label>
                         <input class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="phoneNumber" type="number" placeholder="phoneNumber" onChange = {this.handlerChange} required/>
+                    </div>
+
+                    <div className="mb-6">
+                        <label className="block text-sm font-header mb-2 uppercase" for="idCard">idCard</label>
+                        <input className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white" name="foto" type="file" accept='.png, .jpg, .jpeg' onChange = {this.onFileChange} />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-sm font-header mb-2 uppercase" for="driverLicense">driverLicense</label>
+                        <input className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white" name="driverLicense" type="file" accept='.png, .jpg, .jpeg' onChange = {this.onFileChange} />
                     </div>
                     
 
